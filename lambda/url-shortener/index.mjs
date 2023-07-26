c1960c33babe35b5fb722ec79db9b930eb23e348
davinnev <98953339+davinnev@users.noreply.github.com>
2023-07-27 01:22:35 +0800

    feat(aws): add api gateway

--- lambda/url-shortener/index.mjs
+++ lambda/url-shortener/index.mjs
@@ -3,7 +3,8 @@ import { MongoClient } from "mongodb";
 const client = new MongoClient(process.env.MONGODB_URI);
 
 export const handler = async (event) => {
-  console.log(event);
+  console.log(event.body);
+  const body = JSON.parse(event.body);
   const urlCollection = client.db("TinyUrl").collection("url");
 
   function generateToken() {
@@ -17,16 +18,29 @@ export const handler = async (event) => {
   }
   console.log(urlCollection);
   let token = "";
-  let isTokenAvailable = true;
 
-  while (isTokenAvailable) {
-    generateToken();
-    console.log(token);
-    isTokenAvailable = await urlCollection.findOne({ hash: token });
+  if (!body.customHash) {
+    let isTokenAvailable = true;
+
+    while (isTokenAvailable) {
+      generateToken();
+      console.log(token);
+      isTokenAvailable = await urlCollection.findOne({ hash: token });
+    }
+  } else {
+    token = body.customHash;
   }
 
-  urlCollection.insertOne({ hash: token, url: event.url });
+  urlCollection.insertOne({ hash: token, url: body.url });
 
-  console.log(isTokenAvailable);
   console.log(token);
+
+  return {
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify({
+      success: true,
+      message: "Successfully insert token.",
+    }),
+  };
 };
